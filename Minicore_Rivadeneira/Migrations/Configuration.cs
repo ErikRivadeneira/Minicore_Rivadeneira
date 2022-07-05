ffe237be82578cbb3b47c5ce9726665c7b74d018
ErikRivadeneira <erik.rivadeneira@udla.edu.ec>
2022-07-05 04:28:59 -0500

    Agregar archivos de proyecto.

---
+++ Minicore_Rivadeneira/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Minicore_Rivadeneira.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Minicore_Rivadeneira.Models.miniCoreDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Minicore_Rivadeneira.Models.miniCoreDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Minicore_Rivadeneira/Models/miniCoreDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace Minicore_Rivadeneira.Models
+{
+    public class miniCoreDBContext : DbContext
+    {
+        public DbSet<Pase> Pases { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
